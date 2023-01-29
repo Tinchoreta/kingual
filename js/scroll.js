a8fedffaf231554712cf8d8d1b0bb7381e9dee72
Martin Reta <tinchoreta@gmail.com>
2023-01-29 20:59:48 -0300

    Funciones de scroll en navbar

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+let ubicacionPrincial = window.scrollY
+let $nav = document.querySelector('#nav')
+
+window.addEventListener('scroll', function(){
+    let ubicacionActual = this.window.scrollY
+    console.log(ubicacionActual)
+
+    if(ubicacionPrincial>=ubicacionActual){
+        $nav.style.top = "0px"
+    }
+    else {
+        //oculta el tamaño del header/nav
+        $nav.style.top = "-157px"
+    }    
+    
+})
