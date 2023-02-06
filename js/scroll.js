50e6ee5ffb42910190fc2d5917a4ed825486f2c8
Martin Reta <tinchoreta@gmail.com>
2023-02-06 00:31:12 -0300

    navbar nuevo con animacion

--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,32 @@
-let ubicacionPrincial = window.scrollY;
-let $nav = document.querySelector('#nav');
+/* --pocicion inicial */
+let ubicacionPrincipal = window.pageYOffset;
+let $nav = document.querySelector("nav");
 
-window.addEventListener('scroll', function(){
-    let ubicacionActual = this.window.scrollY
-    console.log(ubicacionActual)
+/* --evento scroll */
+window.addEventListener("scroll", function() {
+    /* --muestra la ubicacion cada vez que hagas scroll */
+    //console.log(window.pageYOffset);
 
-    if(ubicacionPrincial>=ubicacionActual){
-        $nav.style.top = "0px"
+    /* --donde nos encontramos actualmente */
+    let desplazamientoActual = window.pageYOffset;
+
+    /* --condicon para ocultar o mostrar el menu */
+    if(ubicacionPrincipal >= desplazamientoActual) {
+        /* --si es mayor o igual se muesta */
+        $nav.style.top = "0px";
+        console.log('Ubicacion Principal')
+        console.log(ubicacionPrincipal)
+        console.log('desplazamiento')
+        console.log(desplazamientoActual)
+    } else {
+        /* --sino lo ocultamos añadiendo un top negativo */
+        $nav.style.top = "-80px";
+        console.log('Ubicacion Principal')
+        console.log(ubicacionPrincipal)
+        console.log('desplazamiento')
+        console.log(desplazamientoActual)
     }
-    else {
-        //oculta el tamaño del header/nav
-        $nav.style.top = "-px"
-    }    
+
+    /* --actulizamos la ubicacion principal */
     ubicacionPrincipal = desplazamientoActual;
 });
