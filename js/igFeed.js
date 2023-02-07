e4a1fa0c38a0ca751e86af3c38b08652d2195f4e
Martin Reta <tinchoreta@gmail.com>
2023-02-06 23:45:24 -0300

    Ig en javascript file aparte

---
+++ js/igFeed.js
@@ -0,0 +1,7 @@
+var userFeed = new Instafeed({
+    get: 'tinchoreta',
+    target: "instafeed-container",
+    resolution: 'low_resolution',
+    accessToken: 'IGQVJVTVFCUmFodE9uTGcyNHQzUUdndXRNNWNkM2JMUm9RdzU1ZAWlweU1HMHdYSTBTNE11MHZAXVFZAFQ2s1cmpvT3hWUGpLWmFZAVG0xcnJLcldreGNtcndad2dWam5EY3YwSDlZAQldTOFJLZAHFtOHQyXwZDZD'
+});
+userFeed.run();
